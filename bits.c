91635534bae7ca2b863633a5dd8a1b63ed5cfff0
Sage Thomas <sage.thomas@outlook.com>
2016-09-09 12:40:12 -0600

    bitCount works

--- bits.c
+++ bits.c
@@ -344,7 +344,23 @@ int abs(int x) {
  *   Rating: 4
  */
 int bitCount(int x) {
-  return 2;
+  /* counts each byte, then adds each count from each byte */
+  int mask = 1 + (1 << 8) + (1 << 16) + (1 << 24);
+  int count = x & mask;
+  count += (x >> 1) & mask;
+  count += (x >> 2) & mask;
+  count += (x >> 3) & mask;
+  count += (x >> 4) & mask;
+  count += (x >> 5) & mask;
+  count += (x >> 6) & mask;
+  count += (x >> 7) & mask;
+  
+  mask = 255;
+  int sum = count & mask;
+  sum += (count >> 8) & mask;
+  sum += (count >> 16) & mask;
+  sum += (count >> 24) & mask;
+  return sum;
 }
 /* 
  * isNonZero - Check whether x is nonzero using
--- test.c
+++ test.c
@@ -1,13 +1,29 @@
 #include "stdio.h"
 
-int test(int x, int n);
+int test(int x);
 
 int main() {
-  printf("%x\n", test(5,3));
+  printf("%x\n", test(60));
   return 0;
 }
 
-int test(int highbit, int lowbit)
+int test(int x)
 {
-	return 1 << 31;
+  /* counts each byte, then adds each count from each byte */
+  int mask = 1 + (1 << 8) + (1 << 16) + (1 << 24);
+  int count = x & mask;
+  count += (x >> 1) & mask;
+  count += (x >> 2) & mask;
+  count += (x >> 3) & mask;
+  count += (x >> 4) & mask;
+  count += (x >> 5) & mask;
+  count += (x >> 6) & mask;
+  count += (x >> 7) & mask;
+  
+  mask = 255;
+  int sum = count & mask;
+  sum += (count >> 8) & mask;
+  sum += (count >> 16) & mask;
+  sum += (count >> 24) & mask;
+  return sum;
 }
